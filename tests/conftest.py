25b26a4d3bca6d6cdc24eb07c4c712b02e764de7
Robert Samuel <samuelr@aldenv180.allegheny.edu>
2019-04-02 15:18:18 -0400

    Conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,7 @@
 """Configuration file for the test suite"""
-
 import os
 import sys
 
 # Cureently sets the system to contain the previous directory
+MYPATH = os.path.dirname(os.path.asbath(__file__))
+sys.path.insert(0, MYPATH + "/../")
