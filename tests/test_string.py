57a05ab12a6db558f811c74103c4c9fe6e847aad
Megan Corletti <corlettim@aldenv188.allegheny.edu>
2019-04-09 16:22:33 -0400

    start to print all lines of code

---
+++ tests/test_string.py
@@ -0,0 +1,17 @@
+"""Test to print out every line of Python code"""
+
+# print every line of code
+# print to prove
+
+if data.file("test_"):
+    file = open("test_")
+    for line in file
+    print line
+file.close()
+
+
+
+f = open("test_")
+lines = f.readlines()
+for line in lines:
+    print line
