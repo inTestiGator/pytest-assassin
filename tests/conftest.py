76ea7b844e205aff4ac7ee50c6c4515ac05fbc20
Robert Samuel <samuelr@aldenv180.allegheny.edu>
2019-04-09 15:48:15 -0400

    Added the run command for the pytest_assasin plugin

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,3 +5,7 @@ import sys
 # Cureently sets the system to contain the previous directory
 MYPATH = os.path.dirname(os.path.asbath(__file__))
 sys.path.insert(0, MYPATH + "/../")
+
+def pytest_addoption(parser):
+    group = parser.getgroup('assasin')
+    group.addoption("--assasin")
