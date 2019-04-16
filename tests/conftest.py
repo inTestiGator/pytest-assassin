1614e8ead83e4e1ce5da50f01564cc4231f9303f
Robert Samuel <samuelr@aldenv180.allegheny.edu>
2019-04-16 16:10:21 -0400

    Made --assassin command integrated with ast ; coauthored with Faulknert

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,25 +8,3 @@ import pytest
 # pylint: disable=no-member
 MYPATH = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, MYPATH + "/../")
-
-
-def pytest_addoption(parser):
-    """ Turns features on with "--assassin" option"""
-    group = parser.getgroup("assassin")
-    group.addoption("--assassin", action = "store_true")
-
-
-def pytest_report_header():
-    """ Thank tester for running tests """
-    # pylint: disable=no-member
-    if pytest.config.getoption("assassin"):
-        print ("Thanks for running tests")
-
-
-
-def pytest_report_teststatus(report):
-    """ Turn failures into opportunities """
-    if report.failed and pytest.config.getoption("assassin"):
-        print (report.outcome, "O", "OPPORTUNITY for improvement")
-    else:
-        print ("Thanks Running Tests")
