265e34a737be98b25d3a032f52b1d8f43db97002
Megan Corletti <corlettim@aldenv188.allegheny.edu>
2019-04-29 13:56:37 -0400

    all comments in pytest_assassin fix

--- pytest_assassin.py
+++ pytest_assassin.py
@@ -30,6 +30,7 @@ def pytest_report_header():
 #             print("Not a test folder")
 
 def execution():
+    """ Checks for the instance of an assert and prints pass or fail """
     testerFile = open("tests/test_new.py", "r")
     nodes = [
         item
@@ -49,7 +50,7 @@ def execution():
                 print("Fail")
 
 def pytest_collection_modifyitems(items):
-    """ Changes what tests are executed """
+    """ Changes what tests are executed by removing those without an assert """
     nick = []
     for item in items[:]:
         # itemz = inspect.getsource(item)
